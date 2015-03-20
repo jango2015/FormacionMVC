99335aeedf29e1d59e0494d402c0be6e368f9089
cherrera20 <chrishj20@hotmail.com>
2015-03-20 14:25:21 +0100

    prueba1

--- ProyectoPadre/ProyectoJAR/src/main/java/org/ProyectoJAR/App.java
+++ ProyectoPadre/ProyectoJAR/src/main/java/org/ProyectoJAR/App.java
@@ -7,7 +7,8 @@ package main.java.org.ProyectoJAR;
 public class App 
 {
     public static void main( String[] args )
+    
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World2!" );
     }
 }
